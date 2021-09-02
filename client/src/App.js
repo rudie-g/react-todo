e1225318c43328869df4d1597834cfc96ccf622f
Rudie G <rauoldm@gmail.com>
2021-09-02 18:59:43 -0500

    refactored all state into app

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,16 @@ import React, { useState } from 'react'
 
 function App() {
   const [ todoList, setTodoList ] = useState([])
+  const [ todo, setTodo ] = useState("");
   const addTodo = (todo) => {
-    const tempList = [...todoList]
-    tempList.push(todo)
-    setTodoList(tempList)
+    const tempTodo = {name: todo, completed: false}
+    setTodoList([...todoList, tempTodo])
+    setTodo("");
   }
   return (
     <div className="App">
       <Header />
-      <Form addTodo={addTodo} />
+      <Form addTodo={addTodo} todo={todo} setTodo={setTodo} />
     </div>
   );
 }
--- client/src/components/form/index.js
+++ client/src/components/form/index.js
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 export default function Form(props) {
-    const [ todo, setTodo ] = useState("");
+
+    const { todo, setTodo, addTodo } = props;
 
     return (
         <div>
             <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} />
-            <button onClick={props.addTodo(todo)}>Add Todo</button>
+            <button onClick={(e) => {e.preventDefault(); addTodo(todo);}}>Add Todo</button>
         </div>
     )
 }
